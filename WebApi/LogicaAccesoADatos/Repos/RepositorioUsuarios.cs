d94e73cc01f0c22f47ff96488ad291a68dde10ec
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-22 22:16:13 -0300

    Optimiza consulta en FindByEmail con AsNoTracking
    Se ha modificado el método `FindByEmail` en la clase `RepositorioUsuarios` para mejorar el rendimiento. Ahora se utiliza `AsNoTracking()` antes de `AsEnumerable()` al consultar usuarios por correo electrónico, evitando el rastreo de entidades por el contexto de Entity Framework en escenarios donde no se necesitan modificaciones.

--- WebApi/LogicaAccesoADatos/Repos/RepositorioUsuarios.cs
+++ WebApi/LogicaAccesoADatos/Repos/RepositorioUsuarios.cs
@@ -49,7 +49,7 @@ namespace LogicaAccesoADatos.Repos
 
         public Usuario FindByEmail(string email)
         {
-            Usuario? buscado = Context.Usuarios.AsEnumerable().Where(Usuario => Usuario.Email.Email == email).SingleOrDefault();
+            Usuario? buscado = Context.Usuarios.AsNoTracking().AsEnumerable().Where(Usuario => Usuario.Email.Email == email).SingleOrDefault();
             return buscado;
         }
 
