6b3883decfc169d9e50e86f9f94330bf334cf88b
AnisKiki <nadyrmiguel77@gmail.com>
2025-06-23 02:41:19 -0300

    Añadido UsuariosController con métodos Login y Logout
    Se añadió la clase `UsuariosController` en el espacio de nombres
    `WebMVC.Controllers`, con soporte para autenticación de usuarios
    y gestión de sesiones.
    
    - Declarados espacios de nombres necesarios para controladores,
      serialización JSON y DTOs de usuarios.
    - Propiedad `URLApi` añadida para almacenar la URL de la API.
    - Constructor implementado para obtener la configuración de la URL
      desde `IConfiguration`.
    - Método `Login()` creado con soporte para solicitudes HTTP GET y
      POST, validación de modelo, autenticación contra la API y manejo
      de errores.
    - Método `Logout()` añadido para gestionar el cierre de sesión.

---
+++ WebMVC/WebMVC/Controllers/UsuariosController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebMVC.DTOs.Usuarios;
+
+namespace WebMVC.Controllers
+{
+    public class UsuariosController: Controller
+    {
+        public string URLApi { get; set; }
+
+        public UsuariosController(IConfiguration config)
+        {
+            URLApi = config.GetValue<string>("URLApiUsuarios");
+        }
+
+        //GET /api/Usuarios/login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dto);
+            }   
+            try
+            {
+                var respuesta = AuxiliarClienteHttp.EnviarSolicitud(URLApi + "login", "post", dto, null);
+                string body = AuxiliarClienteHttp.ObtenerBody(respuesta);
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    LoginDTO us = JsonConvert.DeserializeObject<LoginDTO>(body);
+                    HttpContext.Session.SetString("logEmail", us.Email);
+                    HttpContext.Session.SetString("LogeadoPass", us.Pass);
+                    return RedirectToAction("Index", "Home");
+                }
+                else if((int)respuesta.StatusCode == 401)
+                {
+                    ViewBag.Error = "Usuario o contraseña incorrectos";
+                }
+                else
+                {
+                    ViewBag.Error = $"Error Api: {respuesta.StatusCode} - {body}";
+                }
+                //return View(dto);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Error";
+            }
+            return View(dto);
+        }
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
