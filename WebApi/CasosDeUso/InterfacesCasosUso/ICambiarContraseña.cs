698abeaccd0aef0bc649204122c879cc56d0c1da
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-22 21:41:43 -0300

    Interface cambiar contraseña

---
+++ WebApi/CasosDeUso/InterfacesCasosUso/ICambiarContraseña.cs
@@ -0,0 +1,15 @@
+﻿using CasosDeUso.DTOs.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasosDeUso.InterfacesCasosUso
+{
+    public interface ICambiarContraseña
+    {
+        public void CambiarContraseña(CambioContrasenaDTO datos);
+
+    }
+}
