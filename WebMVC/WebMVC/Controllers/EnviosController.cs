07bb56a6394db92af94f2084dfba8b3052d31b78
AnisKiki <nadyrmiguel77@gmail.com>
2025-06-23 02:41:06 -0300

    Eliminación de RF1 y método Seguimiento en EnviosController
    Se eliminó el bloque de comentarios que describía el requerimiento
    funcional RF1, el cual permitía consultar detalles de un envío sin
    autenticación mediante su número de tracking. También se eliminó el
    método `Seguimiento`, que mostraba una vista para ingresar el número
    de tracking. Estos cambios reflejan que dicha funcionalidad ya no es
    necesaria o ha sido reubicada en el código.

--- WebMVC/WebMVC/Controllers/EnviosController.cs
+++ WebMVC/WebMVC/Controllers/EnviosController.cs
@@ -15,12 +15,6 @@ namespace WebMVC.Controllers
             URLApi = config.GetValue<string>("UrlApiEnvios");
         }
 
-        //RF1 – Dado u número de tracking de un envío obtener todos los detalles de ese envío (Sin autenticación)
-        //Se ingresará el número de tracking y obtendrá todo el detalle del envío, incluyendo los seguimientos que se le
-        //han realizado.Este requerimiento no debe estar restringido.Se espera que funcione como una forma rápida de
-        //consultar el estado de un envío
-
-
         //GET envios
         //vista para ingresar el numero de tracking F1
         public ActionResult Seguimiento()
