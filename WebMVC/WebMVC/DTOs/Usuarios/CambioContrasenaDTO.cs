bae676063deb2f8984ba84d9c371cef016959a00
AnisKiki <nadyrmiguel77@gmail.com>
2025-06-23 19:34:39 -0300

    Añadir clase CambioContrasenaDTO con validación
    Se agregó la clase `CambioContrasenaDTO` en el archivo `CambioContrasenaDTO.cs` dentro del espacio de nombres `WebMVC.DTOs.Usuarios`.
    La clase implementa la interfaz `IValidable` y contiene las propiedades públicas `Email`, `PassVieja` y `PassNueva`.
    Se añadió el método `Validar()` para verificar que las propiedades no estén vacías, lanzando una excepción en caso contrario.
    Se incluyeron declaraciones de `using` necesarias para los espacios de nombres utilizados.

---
+++ WebMVC/WebMVC/DTOs/Usuarios/CambioContrasenaDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMVC.Interfaces;
+
+namespace WebMVC.DTOs.Usuarios
+{
+    public class CambioContrasenaDTO: IValidable
+    {
+        public string Email { get; set; }
+        public string PassVieja { get; set; }
+        public string PassNueva { get; set; }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new Exception("El Email no puede quedar vacio.");
+            }
+            if (string.IsNullOrEmpty(PassVieja))
+            {
+                throw new Exception("La contraseña no puede quedar vacio.");
+            }
+            if (string.IsNullOrEmpty(PassNueva))
+            {
+                throw new Exception("La nueva contraseña no puede quedar vacio.");
+            }
+        }
+    }
+}
