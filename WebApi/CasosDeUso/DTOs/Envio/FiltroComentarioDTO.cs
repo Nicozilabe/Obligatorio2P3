7d2d1f230cfd87b24be0618c74b45ed2dedd79f0
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-23 19:28:22 -0300

    Añadido filtrado de envíos por comentario
    Se añadió el método `getEnviosByComentario(FiltroComentarioDTO datos)` en la interfaz `IObtenerEnvio` para obtener envíos filtrados por comentario.
    
    En el repositorio (`RepositorioEnvios.cs`), se implementó el método `FindAllLightByComentario(string eCliente, string comentario)` para buscar envíos asociados a un cliente y con comentarios coincidentes parcialmente.
    
    Se creó la clase `FiltroComentarioDTO` para validar los datos de filtro, asegurando que las propiedades `Email` y `Comentario` sean válidas antes de realizar la operación.
    
    Se implementó la funcionalidad en `ObtenerEnvio.cs` para ordenar los envíos por fecha de registro y lanzar una excepción si no se encuentran resultados.
    
    Actualizada la interfaz `IRepositorioEnvios` para definir el método de búsqueda por comentario en el repositorio.

---
+++ WebApi/CasosDeUso/DTOs/Envio/FiltroComentarioDTO.cs
@@ -0,0 +1,28 @@
+﻿using CasosDeUso.InterfacesCasosUso;
+using ExcepcionesPropias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasosDeUso.DTOs.Envio
+{
+    public class FiltroComentarioDTO:IValidable
+    {
+        public string Email { get; set; }
+        public string Comentario { get; set; }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new DatosInvalidosException("El Email-Envio no puede quedar vacio.");
+            }
+            if (string.IsNullOrEmpty(Comentario))
+            {
+                throw new DatosInvalidosException("El Email-Envio no puede quedar vacio.");
+            }
+        }
+    }
+}
