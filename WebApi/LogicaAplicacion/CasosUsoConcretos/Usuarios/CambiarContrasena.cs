7e662e58c48f063105f3958208700ddd766adb5b
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-22 21:42:27 -0300

    Añadir clase CambiarContrasena para gestión de contraseñas
    Se añadió la clase `CambiarContrasena` en `CambiarContrasena.cs`
    dentro del espacio de nombres `LogicaAplicacion.CasosUsoConcretos.Usuarios`.
    La clase implementa la interfaz `ICambiarContraseña` y permite gestionar
    el cambio de contraseña de usuarios mediante validaciones y actualizaciones
    en el repositorio.
    
    Se incluyó una propiedad `Repo` de tipo `IRepositorioUsuarios` y un
    constructor para inicializarla. Se implementó el método `CambiarContraseña`,
    que realiza validaciones como evitar que la nueva contraseña sea igual a la
    anterior, verificar la existencia del usuario, y validar la contraseña actual
    antes de actualizarla. Se lanzan excepciones específicas en caso de errores.
    
    Además, se añadieron dependencias necesarias mediante `using`.

---
+++ WebApi/LogicaAplicacion/CasosUsoConcretos/Usuarios/CambiarContrasena.cs
@@ -0,0 +1,57 @@
+﻿using CasosDeUso.DTOs.Usuarios;
+using CasosDeUso.InterfacesCasosUso;
+using ExcepcionesPropias;
+using LogicaAplicacion.Mapeadores.Usuarios;
+using LogicaNegocio.EntidadesDominio.Usuarios;
+using LogicaNegocio.InterfacesRepositorio;
+using LogicaNegocio.ValueObjects.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosUsoConcretos.Usuarios
+{
+    public class CambiarContrasena : ICambiarContraseña
+    {
+
+        public IRepositorioUsuarios Repo { get; set; }
+        public CambiarContrasena(IRepositorioUsuarios repo)
+        {
+            Repo = repo;
+        }
+        public void CambiarContraseña(CambioContrasenaDTO datos)
+        {
+            
+            datos.Validar();
+
+            if (datos.PassVieja == datos.PassNueva)
+            {
+                throw new DatosInvalidosException("La contraseña nueva no puede ser igual a la anterior.");
+            }
+
+            Usuario buscado = Repo.FindByEmail(datos.Email);
+            if (buscado != null)
+            {
+                if (buscado.Password.Password == datos.PassNueva)
+                {
+
+                    buscado.Password = new UsuarioPassword(datos.PassNueva);
+
+
+
+                }
+                else
+                {
+                    throw new DatosInvalidosException("La contraseña actual no es correcta");
+                }
+
+            }
+            else
+            {
+                throw new DatosInvalidosException("El usuario no existe.");
+            }
+        }
+    }
+}
