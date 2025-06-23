d2d9a6fa8ccbbe250e6cd995108aa73de6b2b7d6
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-22 21:23:07 -0300

    Añadir cambio de contraseña en UsuariosController
    Se ha añadido la acción `CambiarContrasena` al controlador
    `UsuariosController`, protegida con `[Authorize(Roles ="Cliente")]`
    para permitir que los usuarios con rol "Cliente" cambien su contraseña.
    La acción valida los datos recibidos mediante un objeto
    `CambioContrasenaDTO` y maneja excepciones para garantizar respuestas
    adecuadas al cliente.
    
    Se creó la clase `CambioContrasenaDTO` con las propiedades `Email`,
    `PassVieja` y `PassNueva`, además de un método `Validar` que verifica
    los datos y lanza una excepción `DatosInvalidosException` si son
    inválidos.
    
    Se añadieron referencias necesarias y bloques `try-catch` en el
    controlador para manejar errores durante la validación y el proceso
    de cambio de contraseña.

---
+++ WebApi/CasosDeUso/DTOs/Usuarios/CambioContrasenaDTO.cs
@@ -0,0 +1,37 @@
+﻿using CasosDeUso.InterfacesCasosUso;
+using ExcepcionesPropias;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasosDeUso.DTOs.Usuarios
+{
+    public class CambioContrasenaDTO: IValidable
+    {
+
+        public string Email { get; set; }
+        
+        public string PassVieja { get; set; }
+
+        public string PassNueva { get; set; }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new DatosInvalidosException("El Email no puede quedar vacio.");
+            }
+            if (string.IsNullOrEmpty(PassVieja))
+            {
+                throw new DatosInvalidosException("La contraseña no puede quedar vacio.");
+            }
+            if (string.IsNullOrEmpty(PassNueva))
+            {
+                throw new DatosInvalidosException("La nueva contraseña no puede quedar vacio.");
+            }
+        }
+    }
+}
