f19a96f4fcd6c096e5d7d571028ba42630a0f855
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 16:55:03 -0300

    Refactorización en lógica de getEnviosByComentario
    Se añadió una referencia al espacio de nombres
    `LogicaNegocio.EntidadesDominio.Envíos` en `ObtenerEnvioByComentario.cs`.
    
    Se modificó la lógica del método `getEnviosByComentario`:
    - Los resultados de `repoEnvios.FindAllLightByComentario` ahora se
      almacenan directamente como `IEnumerable<Envio>`.
    - Se eliminó la conversión inicial a `IEnumerable<EnvioLigthDTO>` y
      la ordenación por `FechaRegistroEnvio`.
    - Se introdujo la variable `ret` para almacenar el resultado final
      como `IEnumerable<EnvioLigthDTO>`.
    - La conversión a `EnvioLigthDTO` se realiza solo si los envíos no
      son nulos.
    - El método ahora retorna la variable `ret` en lugar de `envios`.

--- WebApi/LogicaAplicacion/CasosUsoConcretos/Envios/ObtenerEnvioByComentario.cs
+++ WebApi/LogicaAplicacion/CasosUsoConcretos/Envios/ObtenerEnvioByComentario.cs
@@ -1,6 +1,7 @@
 ﻿using CasosDeUso.DTOs.Envio;
 using CasosDeUso.InterfacesCasosUso;
 using LogicaAplicacion.Mapeadores.Envios;
+using LogicaNegocio.EntidadesDominio.Envíos;
 using LogicaNegocio.InterfacesRepositorio;
 using System;
 using System.Collections.Generic;
@@ -22,15 +23,14 @@ namespace LogicaAplicacion.CasosUsoConcretos.Envios
         public IEnumerable<EnvioLigthDTO> getEnviosByComentario(FiltroComentarioDTO datos)
         {
             datos.Validar();
-            IEnumerable<EnvioLigthDTO> envios = MapperEnvio.ToListEnvioLigthDTO(repoEnvios.FindAllLightByComentario(datos.Email, datos.Comentario));
-
+            IEnumerable<Envio> envios = repoEnvios.FindAllLightByComentario(datos.Email, datos.Comentario);
+            IEnumerable<EnvioLigthDTO> ret = null;
 
             if (envios != null)
             {
-                IEnumerable<EnvioLigthDTO> ordenados = envios.OrderBy(e => e.FechaRegistroEnvio);
-                envios = ordenados;
+                ret = MapperEnvio.ToListEnvioLigthDTO(envios);
             }
-            return envios;
+            return ret;
         }
     }
 }
