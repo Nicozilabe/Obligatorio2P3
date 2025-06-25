47be0306f4407f0de53a33a0873b679d1e65a848
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 15:22:22 -0300

    Añadir interfaz IObtenerEnviosByEmail
    Se ha creado la interfaz `IObtenerEnviosByEmail` en el archivo
    `IObtenerEnviosByEmail.cs`, dentro del espacio de nombres
    `CasosDeUso.InterfacesCasosUso`. La interfaz define el método
    `getEnviosByEmail`, que recibe un parámetro `email` de tipo `string`
    y devuelve un `IEnumerable<EnvioLigthDTO>`. También se han añadido
    las declaraciones de `using` necesarias para su implementación.

---
+++ WebApi/CasosDeUso/InterfacesCasosUso/IObtenerEnviosByEmail.cs
@@ -0,0 +1,14 @@
+﻿using CasosDeUso.DTOs.Envio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasosDeUso.InterfacesCasosUso
+{
+    public interface IObtenerEnviosByEmail
+    {
+        public IEnumerable<EnvioLigthDTO> getEnviosByEmail(string email);
+    }
+}
