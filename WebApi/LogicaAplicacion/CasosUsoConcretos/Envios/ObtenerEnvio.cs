1d987579b9b84a5e43597f2cafaee0379883cd25
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 15:21:04 -0300

    Refactorización de ObtenerEnvio y eliminación de métodos
    Se eliminó el uso de métodos que interactuaban con el repositorio
    para realizar consultas y transformaciones de datos, como
    `getEnviosLightActivos`, `getByTracking`, `getEnviosByEmail` y
    `getEnviosByComentario`.
    
    Se agregó una propiedad `IRepositorioEnvios repoEnvios` con un
    modificador `set` en la clase `ObtenerEnvio` para permitir la
    inyección de dependencias. También se añadió un constructor que
    inicializa esta propiedad.
    
    Se simplificó la clase `ObtenerEnvio` eliminando lógica de
    validación, manejo de excepciones y transformaciones, dejando
    únicamente su estructura básica y la interacción con el repositorio.

--- WebApi/LogicaAplicacion/CasosUsoConcretos/Envios/ObtenerEnvio.cs
+++ WebApi/LogicaAplicacion/CasosUsoConcretos/Envios/ObtenerEnvio.cs
@@ -14,24 +14,12 @@ namespace LogicaAplicacion.CasosUsoConcretos.Envios
 {
     public class ObtenerEnvio : IObtenerEnvio
     {
+
         IRepositorioEnvios repoEnvios { get; set; }
 
         public ObtenerEnvio(IRepositorioEnvios repoEnvios)
         {
             this.repoEnvios = repoEnvios;
-        }
-
-
-        //Cargar menos a la base con datos que no se van a mostrar
-        public IEnumerable<EnvioLigthDTO> getEnviosLightActivos()
-        {
-            IEnumerable<EnvioLigthDTO> envios = MapperEnvio.ToListEnvioLigthDTO(repoEnvios.FindAllLightActivos());
-
-            if (envios == null || envios.Count() == 0)
-            {
-                throw new DatosInvalidosException("No se encontraron envios activos.");
-            }
-            return envios;
         }
 
         public EnvioDTO getByID(int id)
@@ -47,49 +35,9 @@ namespace LogicaAplicacion.CasosUsoConcretos.Envios
                 ret = MapperEnvio.ToDTO(envio);
             }
 
-
             return ret;
 
-
         }
-
-        public EnvioDTO getByTracking(int tracking)
-        {
-            EnvioDTO envio = null;
-
-            Envio en = repoEnvios.FindByTracking(tracking);
-            if (en != null)
-            {
-                envio = MapperEnvio.ToDTO(en);
-            }
-            return envio;
-        }
-
-        public IEnumerable<EnvioLigthDTO> getEnviosByEmail(string email)
-        {
-            IEnumerable<EnvioLigthDTO> envios = MapperEnvio.ToListEnvioLigthDTO(repoEnvios.FindAllLightByEmailCliente(email));
-
-            
-            if (envios != null)
-            {
-                IEnumerable<EnvioLigthDTO> ordenados = envios.OrderBy(e => e.FechaRegistroEnvio);
-                envios = ordenados;
-            }
-            return envios;
-        }
-
-        public IEnumerable<EnvioLigthDTO> getEnviosByComentario(FiltroComentarioDTO datos)
-        {
-            datos.Validar();
-            IEnumerable<EnvioLigthDTO> envios = MapperEnvio.ToListEnvioLigthDTO(repoEnvios.FindAllLightByComentario(datos.Email, datos.Comentario));
-
-
-            if (envios != null)
-            {
-                IEnumerable<EnvioLigthDTO> ordenados = envios.OrderBy(e => e.FechaRegistroEnvio);
-                envios = ordenados;
-            }
-            return envios;
-        }
+ 
     }
 }
