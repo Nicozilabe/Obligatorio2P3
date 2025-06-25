47031b20debe19a4817a9ab1ccd8c927559e4614
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 16:03:20 -0300

    Añadir interfaz IObtenerEnviosByFecha
    Se añadió el archivo `IObtenerEnviosByFecha.cs` con una nueva interfaz pública dentro del espacio de nombres `CasosDeUso.InterfacesCasosUso`.
    La interfaz incluye el método `getEnviosByFecha`, que devuelve un `IEnumerable<EnvioLigthDTO>` y recibe un parámetro de tipo `FiltroFechaDTO`.
    Se agregaron las declaraciones de `using` necesarias para soportar las nuevas funcionalidades.

---
+++ WebApi/CasosDeUso/InterfacesCasosUso/IObtenerEnviosByFecha.cs
@@ -0,0 +1,16 @@
+﻿using CasosDeUso.DTOs.Envio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasosDeUso.InterfacesCasosUso
+{
+    public interface IObtenerEnviosByFecha
+    {
+        public IEnumerable<EnvioLigthDTO> getEnviosByFecha(FiltroFechaDTO datos);
+
+    }
+
+}
