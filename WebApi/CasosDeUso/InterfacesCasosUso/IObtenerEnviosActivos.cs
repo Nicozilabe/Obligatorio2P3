5d276aa9b74937604221f66c999c8e4de0e59fbf
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 15:22:01 -0300

    Añadir interfaz IObtenerEnviosActivos
    Se ha creado la interfaz `IObtenerEnviosActivos` en el archivo
    `IObtenerEnviosActivos.cs`, dentro del espacio de nombres
    `CasosDeUso.InterfacesCasosUso`. La interfaz incluye el método
    `getEnviosLightActivos`, que devuelve un `IEnumerable<EnvioLigthDTO>`.
    También se han añadido las declaraciones de `using` necesarias.

---
+++ WebApi/CasosDeUso/InterfacesCasosUso/IObtenerEnviosActivos.cs
@@ -0,0 +1,14 @@
+﻿using CasosDeUso.DTOs.Envio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasosDeUso.InterfacesCasosUso
+{
+    public interface IObtenerEnviosActivos
+    {
+        public IEnumerable<EnvioLigthDTO> getEnviosLightActivos();
+    }
+}
