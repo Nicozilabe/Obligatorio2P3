bd1fd71f80cbe0c3768f0fdc72c9136f3b6c19fe
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 15:20:48 -0300

    Refactorización: modularización de lógica en envíos
    Se simplificó la clase `ObtenerEnvio` eliminando métodos redundantes y manteniendo únicamente `getByID`. Se añadió la propiedad `repoEnvios` para manejar el repositorio.
    
    Se crearon nuevas interfaces (`IObtenerEnvioByComentario`, `IObtenerEnvioByTracking`, `IObtenerEnviosActivos`, `IObtenerEnviosByEmail`) para separar la lógica de negocio según su propósito.
    
    Se implementaron clases específicas (`ObtenerEnvioByComentario`, `ObtenerEnvioByTracking`, `ObtenerEnviosActivos`, `ObtenerEnviosByEmail`) que encapsulan la lógica de cada interfaz, siguiendo principios de diseño como responsabilidad única y separación de preocupaciones.

--- WebApi/CasosDeUso/InterfacesCasosUso/IObtenerEnvio.cs
+++ WebApi/CasosDeUso/InterfacesCasosUso/IObtenerEnvio.cs
@@ -9,10 +9,8 @@ namespace CasosDeUso.InterfacesCasosUso
 {
     public interface IObtenerEnvio
     {
-        public IEnumerable<EnvioLigthDTO> getEnviosLightActivos();
+
         public EnvioDTO getByID(int id);
-        public EnvioDTO getByTracking(int tracking);
-        public IEnumerable<EnvioLigthDTO> getEnviosByEmail(string email);
-        public IEnumerable<EnvioLigthDTO> getEnviosByComentario(FiltroComentarioDTO datos);
+
     }
 }
