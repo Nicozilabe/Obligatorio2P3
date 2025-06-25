405642009f7b87d8b7601038b9f85dacb327d73b
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 15:22:14 -0300

    Implementar clase ObtenerEnviosActivos y su lógica
    Se agregó la clase `ObtenerEnviosActivos` en el archivo `ObtenerEnviosActivos.cs`, que implementa la interfaz `IObtenerEnviosActivos`.
    
    La clase incluye:
    - Declaraciones de `using` para los espacios de nombres necesarios.
    - Una propiedad `repoEnvios` de tipo `IRepositorioEnvios` para interactuar con el repositorio.
    - Un constructor que inicializa `repoEnvios`.
    - El método `getEnviosLightActivos()`, que utiliza el mapeador `MapperEnvio` para convertir los envíos activos en objetos `EnvioLigthDTO`. Si no se encuentran envíos, lanza una excepción personalizada `DatosInvalidosException`.
    
    Este commit introduce la lógica para obtener y mapear envíos activos.

---
+++ WebApi/LogicaAplicacion/CasosUsoConcretos/Envios/ObtenerEnviosActivos.cs
@@ -0,0 +1,35 @@
+﻿using CasosDeUso.DTOs.Envio;
+using CasosDeUso.InterfacesCasosUso;
+using ExcepcionesPropias;
+using LogicaAplicacion.Mapeadores.Envios;
+using LogicaNegocio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosUsoConcretos.Envios
+{
+    public class ObtenerEnviosActivos : IObtenerEnviosActivos
+    {
+        IRepositorioEnvios repoEnvios { get; set; }
+
+        public ObtenerEnviosActivos(IRepositorioEnvios repoEnvios)
+        {
+            this.repoEnvios = repoEnvios;
+        }
+
+
+        public IEnumerable<EnvioLigthDTO> getEnviosLightActivos()
+        {
+            IEnumerable<EnvioLigthDTO> envios = MapperEnvio.ToListEnvioLigthDTO(repoEnvios.FindAllLightActivos());
+
+            if (envios == null || envios.Count() == 0)
+            {
+                throw new DatosInvalidosException("No se encontraron envios activos.");
+            }
+            return envios;
+        }
+    }
+}
