587dfcc14a876bb7def2ca002ba37685a85267db
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 16:03:31 -0300

    Añadir clase ObtenerEnviosByFecha e implementar lógica básica
    Se ha añadido la clase `ObtenerEnviosByFecha` en el archivo
    `ObtenerEnviosByFecha.cs`, que implementa la interfaz
    `IObtenerEnviosByFecha`. La clase incluye una propiedad
    `repoEnvios` de tipo `IRepositorioEnvios` y un constructor
    para inicializarla. También se ha implementado el método
    `getEnviosByFecha`, que utiliza un objeto `FiltroFechaDTO`
    para filtrar envíos por fecha, llamando al método `Validar()`
    del DTO. Se han importado los espacios de nombres necesarios
    para la implementación.

---
+++ WebApi/LogicaAplicacion/CasosUsoConcretos/Envios/ObtenerEnviosByFecha.cs
@@ -0,0 +1,25 @@
+﻿using CasosDeUso.DTOs.Envio;
+using CasosDeUso.InterfacesCasosUso;
+using LogicaNegocio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosUsoConcretos.Envios
+{
+    public class ObtenerEnviosByFecha : IObtenerEnviosByFecha
+    {
+        IRepositorioEnvios repoEnvios { get; set; }
+
+        public ObtenerEnviosByFecha(IRepositorioEnvios repoEnvios)
+        {
+            this.repoEnvios = repoEnvios;
+        }
+        public IEnumerable<EnvioLigthDTO> getEnviosByFecha(FiltroFechaDTO datos)
+        {
+            datos.Validar();
+        }
+    }
+}
