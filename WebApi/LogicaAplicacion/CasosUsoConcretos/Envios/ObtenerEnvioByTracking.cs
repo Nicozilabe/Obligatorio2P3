3d8244b233c281bf66e0da80e12073c5c409f095
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-25 15:21:52 -0300

    Añadir clase ObtenerEnvioByTracking y su implementación
    Se agregó la clase `ObtenerEnvioByTracking` en el archivo
    `ObtenerEnvioByTracking.cs`, que implementa la interfaz
    `IObtenerEnvioByTracking`.
    
    La clase incluye una propiedad privada `repoEnvios` de tipo
    `IRepositorioEnvios`, inicializada en el constructor, y el método
    `getByTracking(int tracking)` para buscar envíos por número de
    seguimiento.
    
    El método utiliza el repositorio `repoEnvios` y el mapeador
    `MapperEnvio` para convertir objetos `Envio` en `EnvioDTO`. Si no
    se encuentra un envío, devuelve `null`. También se añadieron las
    declaraciones de `using` necesarias para los espacios de nombres
    requeridos.

---
+++ WebApi/LogicaAplicacion/CasosUsoConcretos/Envios/ObtenerEnvioByTracking.cs
@@ -0,0 +1,35 @@
+﻿using CasosDeUso.DTOs.Envio;
+using CasosDeUso.InterfacesCasosUso;
+using LogicaAplicacion.Mapeadores.Envios;
+using LogicaNegocio.EntidadesDominio.Envíos;
+using LogicaNegocio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosUsoConcretos.Envios
+{
+    public class ObtenerEnvioByTracking : IObtenerEnvioByTracking
+    {
+
+        IRepositorioEnvios repoEnvios { get; set; }
+
+        public ObtenerEnvioByTracking(IRepositorioEnvios repoEnvios)
+        {
+            this.repoEnvios = repoEnvios;
+        }
+        public EnvioDTO getByTracking(int tracking)
+        {
+            EnvioDTO envio = null;
+
+            Envio en = repoEnvios.FindByTracking(tracking);
+            if (en != null)
+            {
+                envio = MapperEnvio.ToDTO(en);
+            }
+            return envio;
+        }
+    }
+}
